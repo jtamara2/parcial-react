fbfd9f7d0a345027f6460fff263db3aad500c896
jtamara2 <jtamara1@cuc.edu.co>
2022-04-25 19:45:49 -0500

    preparando archivo para crud

---
+++ src/Formulario.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { firebase } from "./firebase";
+import { nanoid } from "nanoid";
+
+const Formulario = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">PARCIAL REACT</h1>
+      <hr />
+    </div>
+  );
+};
+
+export default Formulario;
